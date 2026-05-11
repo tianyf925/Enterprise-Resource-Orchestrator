c4463cc473fc3314ebdeda6601c4d20d14b96732
tianyf925 <yifei_tian_work@163.com>
2026-05-11 17:04:55 +0800

    feat: frontend scaffold

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Enterprise Resource Orchestrator</h1>
+      <p>Frontend is successfully connected!</p>
+    </div>
+  );
+}
+
+export default App;
